a252aeae6c53c3e21513ad4395e97b10a904b13b
jj326@berkeley.edu <jjeong060@gmail.com>
2015-03-18 05:09:11 -0700

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Julie Jeong on 3/18/15.
+//  Copyright (c) 2015 Julie Jeong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
